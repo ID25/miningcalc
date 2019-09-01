82e0a540f1620e7c25eef357335b18416f1ba363
Eugene Domosedov <xid25x@gmail.com>
2019-09-01 18:52:59 +0300

    Add per hour stats to chart

--- app/services/calc_mining_profit.rb
+++ app/services/calc_mining_profit.rb
@@ -25,8 +25,14 @@ class CalcMiningProfit
   end
 
   def chart_data
-    current_time = DateTime.now
-    data = { current_time.beginning_of_day => 0.0, current_time.end_of_day => calc_profit.round(2) }
+    current_time = DateTime.now.beginning_of_day
+    profit = calc_profit.round(2)
+    per_hour = profit / 24.0
+    data = 0.upto(21).each_with_object({}) do |_i, obj|
+      current_time += 1.hour
+      obj[current_time] = per_hour.round(2)
+      obj
+    end.merge({ current_time.beginning_of_day => per_hour, current_time.end_of_day => profit })
     { name: coin, data: data }
   end
 end
