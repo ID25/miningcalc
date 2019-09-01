4db9ed13f5e021d4d404ee7bf5a2d43a1e32f71b
Eugene Domosedov <xid25x@gmail.com>
2019-09-01 14:29:08 +0300

    Add method for calculating mining profit per day

--- app/services/calc_mining_profit.rb
+++ app/services/calc_mining_profit.rb
@@ -1,17 +1,26 @@
 class CalcMiningProfit
   API_ENDPOINT = 'https://eth.ezil.me/api/'
 
-  attr_reader :coin
+  attr_reader :coin, :coin_data
 
   def initialize(coin)
     @coin = coin
+    @coin_data = CoinConsts.new(coin)
   end
 
   def self.difficulty
-    HTTParty.get("#{API_ENDPOINT}/stats")['nodes'].first['difficulty']
+    HTTParty.get("#{API_ENDPOINT}/stats")['nodes'].first['difficulty'].to_f
   end
 
   def exchange_rate
     Cryptocompare::Price.find(coin, 'USDT')[coin]['USDT']
+  end
+
+  def calc_profit
+    ((coin_data.h / hashrate) * coin_data.block_profit * coin_data.t * exchange_rate) / coin_data.block_time
+  end
+
+  def hashrate
+    self.class.difficulty / coin_data.block_time
   end
 end
